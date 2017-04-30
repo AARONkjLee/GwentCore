294a90878f1d3c0a1c7fdf218d4193cefc1ce840
铿杰 李 <115010177@link.cuhk.edu.cn>
2017-04-30 17:24:02 +0800

    Finished implemrnt of Card class

--- Classes/card.cpp
+++ Classes/card.cpp
@@ -1,2 +1,75 @@
 #include "card.h"
-#include <json/json.h>
+
+
+Card::Card(int id){
+
+}
+
+int Card::getCountLimit(){
+	return this->countLimit;
+}
+
+std::string Card::getPicDir(){
+	switch(cardSet){
+		case NullCSet: 			
+			break;
+		case Northern:
+			return "Northern Realms" + picDir;
+			break;
+		case Nilfgaarian:
+			return "Nilfgaardian Empire" + picDir;
+			break;
+		case Monster:
+			return "Monsters" + picDir;
+			break;
+		case Scoiateal:
+			return "Scoia'tael" + picDir;
+			break;
+		case Neutral:
+			return "Neutrals" + picDir;
+			break;
+	}
+
+}
+
+std::string Card::getCardName(){
+	return name;
+}
+
+std::string Card::getCardDiscription(){
+	return discription;
+}
+
+CardType Card::getCardType(){
+	return this->cardType;
+}
+
+/*return the card set*/
+
+CardSet Card::getSet(){
+	return this->cardSet;
+}
+
+UnitType Card::getCardSet(){
+	return this->unitType;
+}
+
+UnitLevel Card::getUnitLevel(){
+	return this->unitLevel;
+}
+
+SpellType Card::getSpellType(){
+	return this->spellType;
+}
+
+WeatherType Card::getWeatherType(){
+	return this->weatherType;
+}
+
+bool Card::existEx(){
+	return this->exEffect;
+}
+
+void Card::changeStrengthTo(int CStrength){
+	this->strength = CStrength;
+}
--- Classes/card.h
+++ Classes/card.h
@@ -1,5 +1,63 @@
 #ifndef __card_H__ 
 #define __card_H__ 
- 
- 
+
+#include <json/json.h>
+
+enum CardType { NullCType, Unit, Spell };
+enum CardSet {NullCSet, Northern, Nilfgaarian, Monster, Scoiateal, Neutral};
+enum UnitType {NullUType, CloseCombat, RangedCombat, Seige, CloseRangedCombat};
+enum UnitLevel {NullULevel, Ordinary, Hero };
+enum SpellType {NullSType, Weather, Horn, Decoy, Scorch};
+enum WeatherType {NullWType, Bitingfroest, Fog, Rain, ClearWeather};
+enum EffectType {NullEType, Agile, Spy, TightBond, Decoy, Medic, SpellHorn, UnitHorn, SpellScorch, UnitScorch, Muster, MoraleBoost, BitingFrost, Fog, Rain, ClearWeather };
+
+class Card {
+private:
+	int cardID;
+	int countLimit;
+	int strength;
+	std::string picDir;
+	std::string name;
+	std::string discription;
+	CardType cardType;
+	CardSet cardSet;
+	UnitType unitType;
+	UnitLevel unitLevel;
+	SpellType spellType;
+	WeatherType weatherType;
+	std::vector<EffectType> effects;
+	bool exEffect;
+
+public:
+	Card(int id);
+	~Card() {}
+
+/*return the collection limit of this card*/
+	int getCountLimit();
+/*return the direction of the picture*/
+	std::string getPicDir();
+/*return the card name*/
+	std::string getCardName();
+/*return the card type*/
+	std::string getCardDiscription();
+/*return the card type*/
+	CardType getCardType();
+/*return the card set*/
+	CardSet getSet();
+/*return the card set*/
+	UnitType getCardSet();
+/*return the unit level*/
+	UnitLevel getUnitLevel();
+/*return the spell type*/
+	SpellType getSpellType();
+/*return the weather type*/
+	WeatherType getWeatherType();
+/*return whether there is extral effect(this is under constraction)*/
+	bool existEx();
+/*change strenth*/
+	void changeStrengthTo(int CStrength);
+
+
+};
+
 #endif __card_H__ 
