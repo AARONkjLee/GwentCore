ca868463c8b84184b7358853edb2e012113dcd81
Rita <115010096@link.cuhk.edu.cn>
2017-05-08 00:05:28 +0800

    Card collection prototype linked.

---
+++ Classes/CardCollection.h
@@ -0,0 +1,21 @@
+#ifndef _CARDCOLLECTION__H__ 
+#define _CARDCOLLECTION__H__ 
+
+#include "cocos2d.h"
+
+class CardCollection : public cocos2d::Layer
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(CardCollection);
+};
+
+ 
+#endif _CARDCOLLECTION__H__ 
