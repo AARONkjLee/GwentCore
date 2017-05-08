9f761b76cbc24cfda8cd457b9aff7e545d77b1bd
铿杰 李 <115010177@link.cuhk.edu.cn>
2017-05-08 18:20:46 +0800

    Add and finish LeaderCard Class.

---
+++ Classes/leaderCard.cpp
@@ -0,0 +1,134 @@
+#include "leaderCard.h"
+
+
+LeaderCard::LeaderCard(){}//defaul initalization
+
+LeaderCard::LeaderCard(int id){
+	this->reload(id);
+}
+
+void LeaderCard::reload(int id) {
+	std::ifstream cardsPool;
+	cardsPool.open("..//Resources/LeaderCardPoolDatabase.json");
+	// In Card.cpp, direction system is not consist with Cocos. "..//" means upper dir to ($ProjectDir), which is ($SolutionDir)
+	Json::Reader reader;
+	Json::Value cards;
+	if (!reader.parse(cardsPool, cards, false)) {
+		return;
+	}
+	cardsPool.close();
+	Json::Value card = cards[id];
+	this->cardID = card["cardID"].asInt();
+	this->description = card["Description"].asCString();
+	this->name = card["Name"].asCString();
+	this->picDir = card["Pic"].asCString();
+	/*To do: Translate string to enum*/
+	// card type
+	std::string strAbility = card["Ability"].asCString();
+	if (strAbility == " Pick_Rain") {
+		this->ability = Pick_Rain;
+	}
+	else if (strAbility == "Read_Hand_Card") {
+		this->ability = Read_Hand_Card;
+	}
+	else if (strAbility == "Cancel_Ability") {
+		this->ability = Cancel_Ability;
+	}
+	else if (strAbility == "Rraw_From_Opponent") {
+		this->ability = Rraw_From_Opponent;
+	}
+	else if (strAbility == "Restore_Unit") {
+		this->ability = Restore_Unit;
+	}
+	else if (strAbility == "Pick_Fog") {
+		this->ability = Pick_Fog;
+	}
+	else if (strAbility == "Clear_Weathear") {
+		this->ability = Clear_Weathear;
+	}
+	else if (strAbility == "Seige_Horn") {
+		this->ability = Seige_Horn;
+	}
+	else if (strAbility == "Seige_Scorch") {
+		this->ability = Seige_Scorch;
+	}
+	else if (strAbility == "Range_Scorch") {
+		this->ability = Range_Scorch;
+	}
+	else if (strAbility == "Pick_Frost") {
+		this->ability = Pick_Frost;
+	}
+	else if (strAbility == "Draw_11th") {
+		this->ability = Draw_11th;
+	}
+	else if (strAbility == "Range_Horn") {
+		this->ability = Range_Horn;
+	}
+	else if (strAbility == "Close_Scorch") {
+		this->ability = Close_Scorch;
+	}
+	else if (strAbility == "Move_Aglie") {
+		this->ability = Move_Aglie;
+	}
+	else if (strAbility == "Pick_Weather") {
+		this->ability = Pick_Weather;
+	}
+	else if (strAbility == "Close_Horn") {
+		this->ability = Close_Horn;
+	}
+	else if (strAbility == "Destroy2_Draw1") {
+		this->ability = Destroy2_Draw1;
+	}
+	else if (strAbility == "Restore_Unit_ToHand") {
+		this->ability = Restore_Unit_ToHand;
+	}
+	else if (strAbility == "Double_Spies") {
+		this->ability = Double_Spies;
+	}
+
+
+}
+
+
+
+std::string LeaderCard::getPicDir(){
+	// To be called in Cocos, the initial dir is ($ProjectDir)/Resources
+	//return "GwentCardsResources/Monsters/" + picDir;
+	switch(cardSet){
+		case NullCSet: 			
+			break;
+		case Northern:
+			return "GwentCardsResources/Northern Realms/" + picDir;
+			break;
+		case Nilfgaarian:
+			return "GwentCardsResources/Nilfgaardian Empire/" + picDir;
+			break;
+		case Monster:
+			return "GwentCardsResources/Monsters/" + picDir;
+			break;
+		case Scoiateal:
+			return "GwentCardsResources/Scoia'tael/" + picDir;
+			break;
+	}
+
+}
+
+std::string LeaderCard::getName(){
+	return this->name;
+}
+
+std::string LeaderCard::getDescription(){
+	return this->description;
+}
+
+
+/*return the card set*/
+
+CardSet LeaderCard::getSet(){
+	return this->cardSet;
+}
+
+Ability LeaderCard::getAbility()
+{
+	return this->ability;
+}
---
+++ Classes/leaderCard.h
@@ -0,0 +1,63 @@
+#ifndef __card_H__ 
+#define __card_H__ 
+
+#include <json/json.h>
+#include <fstream>
+#include <string>
+
+
+enum CardSet {NullCSet, Northern, Nilfgaarian, Monster, Scoiateal, Neutral};
+enum Ability {
+	Pick_Rain, //Pick a Torrential Rain card from your deck and play it instantly.
+	Read_Hand_Card, //Look at 3 random cards from your opponent's hand.
+	Cancel_Ability, //Cancel your opponent's Leader Ability.
+	Rraw_From_Opponent, //Draw a card from your opponent's discard pile.
+	Restore_Unit, //Abilities that restore a unit to the battlefield restore a randomly-chosen unit. Affects both players.
+	Pick_Fog, //Pick an Impenetrable Fog card from your deck and play it instantly.
+	Clear_Weathear, //Clear any weather effects (resulting from Biting Frost, Torrential Rain or Impenetrable Fog cards) in play.
+	Seige_Horn, //Doubles the strength of all your Siege units (unless a Commander's Horn is also present on that row).
+	Seige_Scorch, //Destroy your enemy's strongest Siege unit(s) if the combined strength of all his or her Siege units is 10 or more.
+	Range_Scorch, //Destroy your enemy's strongest Ranged Combat unit(s) if the combined strength of all his or her Ranged Combat units is 10 or more.
+	Pick_Frost, //Pick a Biting Frost card from your deck and play it instantly.
+	Draw_11th, //Draw an extra card at the beginning of the battle
+	Range_Horn, //Doubles the strength of all your Ranged Combat units (unless a Commander's Horn is also present on that row).
+	Close_Scorch, //Destroy your enemy's Close Combat unit(s) if the combined strength of all his or her Close Combat units is 10 or more.
+	Move_Aglie, //Move agile unites to whichever valid row maximizes their strength (don't move units already in optimal row).
+	Pick_Weather, //Pick any weather card from your deck and play it instantly.
+	Close_Horn, //Double the strength of all your Close Combat units (unless a Commander's Horn is also present on that row).
+	Destroy2_Draw1, //Discard 2 cards and draw 1 card of your choice from your deck.
+	Restore_Unit_ToHand, //Restore a card from your discard pile to your hand.
+	Double_Spies //Doubles strength of all spies.
+};
+
+class LeaderCard {
+private:
+	int cardID;
+	std::string picDir;
+	std::string name;
+	std::string description;
+	CardSet cardSet;
+	Ability ability;
+
+public:
+	LeaderCard();
+	LeaderCard(int id);
+	~LeaderCard() {}
+
+/*load the card using the id*/
+	void reload(int id);
+
+
+/*return the direction of the picture*/
+	std::string getPicDir();
+/*return the card name*/
+	std::string getName();
+/*return the card type*/
+	std::string getDescription();
+/*return the card set*/
+	CardSet getSet();
+/*return the Ability*/
+	Ability getAbility();
+};
+
+#endif __card_H__ 
