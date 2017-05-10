3d9e14f5392569868e55b0e61927dfe546fc502b
Rita <115010096@link.cuhk.edu.cn>
2017-05-10 21:34:16 +0800

    Card collection 1st draft (No functions linked.)

--- Classes/CardCollection.cpp
+++ Classes/CardCollection.cpp
@@ -45,31 +45,75 @@ bool CardCollection::init()
 		origin.y + closeItem->getContentSize().height / 2));
 
 	auto leftLabel1 = MenuItemImage::create(
-		"left_label1.png",
-		"left_label2.png"
+		"label.png",
+		"label.png"
 	);
-	leftLabel1->setPosition(Vec2(origin.x + leftLabel1->getContentSize().width/3,
-		origin.y + visibleSize.height / 10 * 7));
-	leftLabel1->setScale(1.2);
+	leftLabel1->setPosition(Vec2(origin.x + visibleSize.width / 10,
+		origin.y + visibleSize.height / 10 * 8));
+	leftLabel1->setScale(0.7);
 	this->addChild(leftLabel1, 1);
 
 	auto leftLabel2 = MenuItemImage::create(
-		"left_label1.png",
-		"left_label2.png"
+		"label.png",
+		"label.png"
 	);
-	leftLabel2->setPosition(Vec2(origin.x + leftLabel2->getContentSize().width / 3,
-		origin.y + visibleSize.height / 10 * 6));
-	leftLabel2->setScale(1.2);
+	leftLabel2->setPosition(Vec2(origin.x + visibleSize.width / 10,
+		origin.y + visibleSize.height / 10 * 7));
+	leftLabel2->setScale(0.7);
 	this->addChild(leftLabel2, 1);
 
 	auto leftLabel3 = MenuItemImage::create(
-		"left_label1.png",
-		"left_label2.png"
+		"label.png",
+		"label.png"
 	);
-	leftLabel3->setPosition(Vec2(origin.x + leftLabel3->getContentSize().width / 3,
-		origin.y + visibleSize.height / 2 ));
-	leftLabel3->setScale(1.2);
+	leftLabel3->setPosition(Vec2(origin.x + visibleSize.width / 10,
+		origin.y + visibleSize.height / 10 * 6 ));
+	leftLabel3->setScale(0.7);
 	this->addChild(leftLabel3, 1);
+
+	auto buttonb1 = MenuItemImage::create(
+		"buttonb.png",
+		"buttonb.png"
+	);
+	buttonb1->setPosition(Vec2(origin.x + visibleSize.width / 7 * 3,
+		origin.y + visibleSize.height / 11 * 2.7 ));
+	buttonb1->setScale(0.7);
+	this->addChild(buttonb1, 1);
+
+	auto buttonb2 = MenuItemImage::create(
+		"buttonb.png",
+		"buttonb.png"
+	);
+	buttonb2->setPosition(Vec2(origin.x + visibleSize.width / 7 * 4,
+		origin.y + visibleSize.height / 11 * 2.7));
+	buttonb2->setScale(0.7);
+	this->addChild(buttonb2, 1);
+
+	auto buttonb3 = MenuItemImage::create(
+		"buttonb.png",
+		"buttonb.png"
+	);
+	buttonb3->setPosition(Vec2(origin.x + visibleSize.width / 7 * 5,
+		origin.y + visibleSize.height / 11 * 2.7));
+	buttonb3->setScale(0.7);
+	this->addChild(buttonb3, 1);
+
+	auto rightb = MenuItemImage::create(
+		"right.png",
+		"right.png"
+	);
+	rightb->setPosition(Vec2(origin.x + visibleSize.width * 0.644,
+		origin.y + visibleSize.height / 11 * 2.7));
+	this->addChild(rightb, 1);
+
+	auto leftb = MenuItemImage::create(
+		"left.png",
+		"left.png"
+	);
+	leftb->setPosition(Vec2(origin.x + visibleSize.width/ 2,
+		origin.y + visibleSize.height / 11 * 2.7));
+	this->addChild(leftb, 1);
+
 
 	// create menu, it's an autorelease object
 	auto menu = Menu::create(closeItem, NULL);
@@ -82,10 +126,16 @@ bool CardCollection::init()
 
 	// add a label shows "Hello World"
 	// create and initialize a label
-	auto background = Sprite::create("BGP_1920x1080.jpg");
+	auto background = Sprite::create("_BGP_1920x1080.png");
 	background->setPosition(origin.x + visibleSize.width / 2,
 		origin.y + visibleSize.height / 2);
 	this->addChild(background, 0);
+
+	auto back1 = Sprite::create("back1.png");
+	back1->setPosition(origin.x + visibleSize.width / 7 * 4,
+		origin.y + visibleSize.height / 2);
+	back1->setScale(1.5);
+	this->addChild(back1, 0);
 
 	auto label = Label::createWithTTF("Card Collection", "fonts/Marker Felt.ttf", 30);
 
@@ -109,11 +159,22 @@ bool CardCollection::init()
 	// add the sprite as a child to this layer
 	this->addChild(sprite, 0);
 	*/
+	for (int i = 1; i < 7; i++) {
+		auto CardS1 = CardSprite::create(i);
+		CardS1->setPosition(Vec2(visibleSize.width / 9 * (1.587+i) + origin.x, visibleSize.height / 2.53  + origin.y));
+		CardS1->setScale(0.23);
+		this->addChild(CardS1);
 
-	auto CardS1 = CardSprite::create(2);
-	CardS1->setPosition(Vec2(visibleSize.width / 5 + origin.x, visibleSize.height / 4+ origin.y));
-	CardS1->setScale(0.5);
-	this->addChild(CardS1);
+	}
+
+	for (int i = 7; i < 13; i++) {
+		auto CardS1 = CardSprite::create(i);
+	    CardS1->setPosition(Vec2(visibleSize.width / 9 * (1.587 + (i-6)) + origin.x, visibleSize.height / 1.47 + origin.y));
+		CardS1->setScale(0.23);
+		this->addChild(CardS1);
+
+	}
+
 
 
 
