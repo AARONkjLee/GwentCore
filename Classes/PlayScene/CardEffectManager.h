869b9f85dcbcd13a6f6494ae11da6c85660cc10b
Junce <wyqqb@outlook.com>
2017-05-12 03:36:53 +0800

    Add CardEffectManager

---
+++ Classes/PlayScene/CardEffectManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// This file is to define all template card effect
+// These effects are all for "EffectType"
+
+// A effect function should have a input of Field
+// and a input of a Act
+
+/*enum EffectType {NullEType, 
+Agile, 
+Spye, 
+TightBonde, 
+Decoye, 
+Medice, 
+SpellHorne, 
+UnitHorne, 
+SpellScorche, 
+UnitScorche, 
+Mustere, 
+MoraleBooste, 
+BitingFroste, 
+Foge, 
+Raine, 
+ClearWeathere 
+};*/
+
+
