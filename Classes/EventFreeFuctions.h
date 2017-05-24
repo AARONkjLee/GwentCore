35d2201d1a4d68fbf8d291af1739c730007d3d16
Junce <wyqqb@outlook.com>
2017-05-24 21:40:24 +0800

    clickOnTarget Function supports touch or mouse event

--- Classes/EventFreeFuctions.h
+++ Classes/EventFreeFuctions.h
@@ -1,13 +1,11 @@
 #pragma once
 #include "cocos2d.h"
 
-template <typename typeValue>
-bool mouseEventOnTarget(cocos2d::EventMouse* & mouseEvent, typeValue & target) {
-	Vec2 locationInNode = target->convertToNodeSpace(mouseEvent->getLocation());
+template <typename eventTypeValue, typename typeValue>
+bool clickOnTarget(eventTypeValue* & eventOrTouch, typeValue & target) {
+	Vec2 locationInNode = target->convertToNodeSpace(eventOrTouch->getLocation());
 	Size s = target->getContentSize();
 	Rect rect = Rect(0, 0, s.width, s.height);
-
-
 	// µã»÷·¶Î§ÅÐ¶Ï¼ì²â
 	if (rect.containsPoint(locationInNode))
 	{
